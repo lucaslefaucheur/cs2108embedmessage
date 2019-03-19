411c2f824b72323c0be7178423c93613a8f302b6
hcchang0701 <ted341@gmail.com>
2019-03-20 01:46:54 +0800

    add a(amplitude) and f(frequency) for tuning
    It sounds pretty nice now...

--- project.m
+++ project.m
@@ -6,44 +6,43 @@ y = y(1 : Fs*36);
 
 message = 'hello world';
 
-encode(y,Fs,message);
-decode(length(message));
+encode(y,Fs,message, 0.005, 150);
+decode(length(message), 0.005, 150);
 
-function encode(y,Fs,message)
+function encode(y,Fs,message, a, f)
     step = 1/Fs;
     y_encoded = y;
     message = double(message);
     
     for i=1:length(message)
         t = ((i-1)-step:step:i-step);
-        sig = [zeros(1,(i-1)*length(t)) sin(2*pi*100*message(i)*t) zeros(1,length(y)-length(t)*i)];
+        sig = [zeros(1,(i-1)*length(t)) a*sin(2*pi*f*message(i)*t) zeros(1,length(y)-length(t)*i)];
         y_encoded = y_encoded + sig;
     end
     
     audiowrite('edelweiss_36seconds.mp4',y_encoded,Fs);
 end
 
-function decode(n)
+function decode(n, a, f)
     [y, Fs] = audioread('edelweiss_36seconds.mp4');
     
     for i=1:n
         y_decode = y((i-1)*Fs+1 : Fs*i);
         L = Fs*1;
         Y = fft(y_decode);
-        f = Fs*(0:(L/2))/L;
+        fx = Fs*(0:(L/2))/L;
         P2 = abs(Y);
         P1 = P2(1:L/2+1);
         P1(2:end-1) = 2*P1(2:end-1);
         
-        %figure, plot(f, P1)
+        figure, plot(fx, P1)
 
         [pks, locs] = findpeaks(P1);
         for j = 1:length(pks)
-            if pks(j) > 40000 
-                char(round(locs(j)/100))
+            if pks(j) > 40000*a && locs(j) > 32*f
+                locs(j)
+                char(round(locs(j)/f))
             end
         end
     end
 end
-
-
