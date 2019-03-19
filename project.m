b7b9aa13ecf3eb958fa516f9168490de91ef7421
Lucas Le Faucheur <lucaslefaucheur@gmail.com>
2019-03-19 17:48:12 +0800

    added decode function

--- project.m
+++ project.m
@@ -1,47 +1,15 @@
 close all
-clear all
 
-% read audio file
 filename = 'Edelweiss.mp3';
 [y, Fs] = audioread(filename);
 y = y(1 : Fs*36);
 
-step = 1/Fs;
-% random signal 1
-t = (0:step:1-step);
-sig = [sin(2*pi*5000*t) zeros(1,length(y)-length(t))];
-% random signal 2
-t1 = (1-step:step:2-step);
-sig1 = [zeros(1,length(t1)) sin(2*pi*6000*t1) zeros(1,length(y)-length(t1)*2)];
+message = 'hello world';
 
-% add to original audio
-y = encode(y,Fs,'abcde');
+encode(y,Fs,message);
+decode(length(message));
 
-% write to audio file
-audiowrite('edelweiss_36seconds.mp4', y, Fs)
-[y, Fs] = audioread('edelweiss_36seconds.mp4');
-
-y1 = y(1 : Fs*36);
-
-% fft result
-figure
-L = Fs*36;
-Y = fft(y1);
-f = Fs*(0:(L/2))/L;
-P2 = abs(Y);
-P1 = P2(1:L/2+1);
-P1(2:end-1) = 2*P1(2:end-1);
-plot(f, P1)
-
-[sortpks, sortlocs] = findpeaks(P1(4500:end));
-for i = 1:length(sortpks)
-   if sortpks(i) > 40000 
-       %sortpks(i) 
-       char(round(sortlocs(i)/3554))
-   end
-end
-
-function y_encoded = encode(y,Fs,message)
+function encode(y,Fs,message)
     step = 1/Fs;
     y_encoded = y;
     message = double(message);
@@ -50,6 +18,31 @@ function y_encoded = encode(y,Fs,message)
         t = ((i-1)-step:step:i-step);
         sig = [zeros(1,(i-1)*length(t)) sin(2*pi*100*message(i)*t) zeros(1,length(y)-length(t)*i)];
         y_encoded = y_encoded + sig;
+    end
+    
+    audiowrite('edelweiss_36seconds.mp4',y_encoded,Fs);
+end
+
+function decode(n)
+    [y, Fs] = audioread('edelweiss_36seconds.mp4');
+    
+    for i=1:n
+        y_decode = y((i-1)*Fs+1 : Fs*i);
+        L = Fs*1;
+        Y = fft(y_decode);
+        f = Fs*(0:(L/2))/L;
+        P2 = abs(Y);
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+        
+        %figure, plot(f, P1)
+
+        [pks, locs] = findpeaks(P1);
+        for j = 1:length(pks)
+            if pks(j) > 40000 
+                char(round(locs(j)/100))
+            end
+        end
     end
 end
 
